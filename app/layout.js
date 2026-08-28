c057abbafed0f38bc01e33a19ca549bb2e6ae9d7
Trocaê <ianfelixbelial@gmail.com>
2026-08-28 10:44:55 -0300

    Remove Mapbox CSS link from RootLayout
    Removed Mapbox CSS link from the layout.

--- app/layout.js
+++ app/layout.js
@@ -8,13 +8,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <head>
-        {/* CSS Oficial do Mapbox via CDN */}
-        <link
-          href="https://api.mapbox.com/mapbox-gl-js/v3.1.2/mapbox-gl.css"
-          rel="stylesheet"
-        />
-      </head>
       <body className="w-full h-full m-0 p-0 overflow-hidden">{children}</body>
     </html>
   );
