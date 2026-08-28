4ffcabeda29116cd5c59a149ae530082a07669ce
Trocaê <ianfelixbelial@gmail.com>
2026-08-27 23:42:38 -0300

    Refactor layout.js for metadata and body structure
    Updated application metadata and modified layout structure.

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,21 @@
-import "./globals.css";
+import './globals.css';
 
 export const metadata = {
-  title: "App Ônibus Recife",
-  description: "Mobilidade simples em Recife",
+  title: 'App Ônibus Recife',
+  description: 'Transporte público em tempo real',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        <script src="https://cdn.tailwindcss.com"></script>
-        {/* CSS do Leaflet para o mapa real */}
+        {/* CSS Oficial do Mapbox para renderização perfeita */}
         <link
+          href="https://api.mapbox.com/mapbox-gl-js/v3.1.2/mapbox-gl.css"
           rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-          crossOrigin=""
         />
       </head>
-      <body className="bg-slate-900 text-slate-800 antialiased h-full w-full overflow-hidden select-none">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
